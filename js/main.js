0286d3e69716c2bc7919fe02474b7b8c7c519a58
Hinrikus Wolf <mail@hinrikus-wolf.de>
2021-05-15 19:42:05 +0200

    add templates for shelly temperature addon

--- js/main.js
+++ js/main.js
@@ -33,6 +33,8 @@ let clear_pending_indicator = (e) => {
 const updaters = {
     temperature: (e, s, b, f) => { update_float_value(e, f); },
     humidity: (e, s, b, f) => { update_float_value(e, f); },
+    shelly_temperature: (e, s, b, f) => { update_float_value(e, f); },
+    shelly_humidity: (e, s, b, f) => { update_float_value(e, f); },
     soilmoisture: (e, s, b, f) => { update_float_value(e, f); },
     pressure: (e, s, b, f) => { update_float_value(e, f); },
     shelly: (e, s, b, f) => { clear_pending_indicator(e); update_checkbox(e, b); },
